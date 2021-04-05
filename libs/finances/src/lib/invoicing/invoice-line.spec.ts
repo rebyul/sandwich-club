346d256581c9cfffd19b3397b31d354607ea7a17
Rebyul <rebyul@gmail.com>
2021-04-06 11:22:56 +1200

    Add more tests for invoice line initializations. Check invoice line id uniqueness. Change line initialization in Invoice constructor.

--- libs/finances/src/lib/invoicing/invoice-line.spec.ts
+++ libs/finances/src/lib/invoicing/invoice-line.spec.ts
@@ -3,7 +3,19 @@ import { InvoiceLine, InvoiceLineProps } from "./invoice-line";
 
 const newInvoiceLineId = () => UuidTool.newUuid();
 const newInvoiceLineDescription = (value?: string) => value || `Line description ${Math.random() * 100}`
-const newInvoiceLine = (values?: InvoiceLineProps) => new InvoiceLine({ ...values })
+const newInvoiceLine = (values: InvoiceLineProps = { id: newInvoiceLineId(), description: newInvoiceLineDescription() }) => new InvoiceLine({ ...values })
+
+describe('newInvoiceLineId returns unique uuid', () => {
+  const newId1 = newInvoiceLineId();
+  const newId2 = newInvoiceLineId();
+  expect(newId1 === newId2).toBe(false);
+})
+
+describe('newInvoiceLine returns unique invoice line', () => {
+  const newId1 = newInvoiceLine();
+  const newId2 = newInvoiceLine();
+  expect(newId1.equals(newId2)).toBe(false);
+})
 
 describe('invoice lines', () => {
 
--- libs/finances/src/lib/invoicing/invoice.spec.ts
+++ libs/finances/src/lib/invoicing/invoice.spec.ts
@@ -1,5 +1,6 @@
 import { List } from "immutable";
 import { UuidTool } from "uuid-tool"
+import { InvoiceLineExistsError } from "./exceptions";
 import { Invoice } from './invoice';
 import { InvoiceLine } from "./invoice-line";
 import { newInvoiceLine, newInvoiceLineDescription, newInvoiceLineId } from "./invoice-line.spec";
@@ -14,6 +15,14 @@ describe('invoice', () => {
     expect(testInvoice.id).toEqual(testId);
     expect(testInvoice.ownerId).toEqual(testOwnerId);
     expect(testInvoice.createdAt).toBeDefined();
+  })
+
+  it('create new invoice with lines', () => {
+    const testInvoiceLine = new InvoiceLine({ id: newInvoiceLineId(), description: newInvoiceLineDescription() })
+    const testInvoice = new Invoice(testId, testOwnerId, [testInvoiceLine])
+
+    expect(testInvoice.invoiceLines.count()).toBe(1);
+    expect(testInvoice.invoiceLines.find(i => i.equals(testInvoiceLine))).toBe(testInvoiceLine);
   })
 
   it('add single invoice line', () => {
@@ -45,5 +54,24 @@ describe('invoice', () => {
 
     testInvoice.removeInvoiceLine(invoiceLineToRemove);
     expect(testInvoice.invoiceLines.count()).toBe(1);
+  })
+
+  it('direct manipulation to invoice lines does not persist', () => {
+    const testinvoiceLine = newInvoiceLine();
+    const testinvoiceLine2 = newInvoiceLine();
+
+    const testInvoice = new Invoice(testId, testOwnerId, List([testinvoiceLine]));
+    expect(testInvoice.invoiceLines.count()).toBe(1);
+
+    testInvoice.invoiceLines.concat(testinvoiceLine2);
+    expect(testInvoice.invoiceLines.count()).toBe(1);
+  })
+
+  it('adding an existing invoice line throws new error', () => {
+    const testinvoiceLine = newInvoiceLine();
+
+    const testInvoice = new Invoice(testId, testOwnerId, List([testinvoiceLine]));
+    expect(testInvoice.invoiceLines.count()).toBe(1);
+    expect(() => testInvoice.addInvoiceLine(testinvoiceLine)).toThrow(InvoiceLineExistsError);
   })
 })
