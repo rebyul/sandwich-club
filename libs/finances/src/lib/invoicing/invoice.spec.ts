8507768cf4df3c70cfc2d923a59ee293c3641e99
Rebyul <rebyul@gmail.com>
2021-04-07 11:41:45 +1200

    Configure prettier rules in eslint

--- libs/finances/src/lib/invoicing/invoice.spec.ts
+++ libs/finances/src/lib/invoicing/invoice.spec.ts
@@ -1,60 +1,82 @@
-import { List } from "immutable";
-import { UuidTool } from "uuid-tool"
-import { InvoiceLineExistsError } from "./exceptions";
+import { List } from 'immutable';
+import { UuidTool } from 'uuid-tool';
+import { InvoiceLineExistsError } from './exceptions';
 import { Invoice } from './invoice';
-import { InvoiceLine } from "./invoice-line";
-import { newInvoiceLine, newInvoiceLineDescription, newInvoiceLineId } from "./invoice-line.spec";
+import { InvoiceLine } from './invoice-line';
+import { newInvoiceLine, newInvoiceLineDescription, newInvoiceLineId } from './invoice-line.spec';
 
 describe('invoice', () => {
   const testId = UuidTool.newUuid();
   const testOwnerId = UuidTool.newUuid();
   it('create new invoice', () => {
-    const testInvoice = new Invoice(testId, testOwnerId)
+    const testInvoice = new Invoice(testId, testOwnerId);
     expect(testInvoice).toBeTruthy();
     expect(testInvoice instanceof Invoice).toBe(true);
     expect(testInvoice.id).toEqual(testId);
     expect(testInvoice.ownerId).toEqual(testOwnerId);
     expect(testInvoice.createdAt).toBeDefined();
-  })
+  });
 
   it('create new invoice with lines', () => {
-    const testInvoiceLine = new InvoiceLine({ id: newInvoiceLineId(), description: newInvoiceLineDescription() })
-    const testInvoice = new Invoice(testId, testOwnerId, [testInvoiceLine])
+    const testInvoiceLine = new InvoiceLine({
+      id: newInvoiceLineId(),
+      description: newInvoiceLineDescription(),
+    });
+    const testInvoice = new Invoice(testId, testOwnerId, [testInvoiceLine]);
 
     expect(testInvoice.invoiceLines.count()).toBe(1);
-    expect(testInvoice.invoiceLines.find(i => i.equals(testInvoiceLine))).toBe(testInvoiceLine);
-  })
+    expect(testInvoice.invoiceLines.find((i) => i.equals(testInvoiceLine))).toBe(testInvoiceLine);
+  });
 
   it('add single invoice line', () => {
-    const testInvoice = new Invoice(testId, testOwnerId)
-    const testInvoiceLine = new InvoiceLine({ id: newInvoiceLineId(), description: newInvoiceLineDescription() })
+    const testInvoice = new Invoice(testId, testOwnerId);
+    const testInvoiceLine = new InvoiceLine({
+      id: newInvoiceLineId(),
+      description: newInvoiceLineDescription(),
+    });
     testInvoice.addInvoiceLine(testInvoiceLine);
 
-    expect(testInvoice.invoiceLines.findIndex(l => l.equals(testInvoiceLine))).toBeGreaterThan(-1);
+    expect(testInvoice.invoiceLines.findIndex((l) => l.equals(testInvoiceLine))).toBeGreaterThan(
+      -1
+    );
     expect(testInvoice.invoiceLines.count()).toBe(1);
-  })
+  });
 
   it('add multiple invoice lines', () => {
-    const testInvoice = new Invoice(testId, testOwnerId)
-    const testInvoiceLine1 = new InvoiceLine({ id: newInvoiceLineId(), description: newInvoiceLineDescription() })
-    const testInvoiceLine2 = new InvoiceLine({ id: newInvoiceLineId(), description: newInvoiceLineDescription() })
+    const testInvoice = new Invoice(testId, testOwnerId);
+    const testInvoiceLine1 = new InvoiceLine({
+      id: newInvoiceLineId(),
+      description: newInvoiceLineDescription(),
+    });
+    const testInvoiceLine2 = new InvoiceLine({
+      id: newInvoiceLineId(),
+      description: newInvoiceLineDescription(),
+    });
     testInvoice.addInvoiceLines([testInvoiceLine1, testInvoiceLine2]);
 
-    expect(testInvoice.invoiceLines.findIndex(l => l.equals(testInvoiceLine1))).toBeGreaterThan(-1);
-    expect(testInvoice.invoiceLines.findIndex(l => l.equals(testInvoiceLine2))).toBeGreaterThan(-1);
+    expect(testInvoice.invoiceLines.findIndex((l) => l.equals(testInvoiceLine1))).toBeGreaterThan(
+      -1
+    );
+    expect(testInvoice.invoiceLines.findIndex((l) => l.equals(testInvoiceLine2))).toBeGreaterThan(
+      -1
+    );
     expect(testInvoice.invoiceLines.count()).toBe(2);
-  })
+  });
 
   it('remove single invoice line', () => {
     const testinvoiceLine = newInvoiceLine();
     const invoiceLineToRemove = newInvoiceLine();
-    const testInvoice = new Invoice(testId, testOwnerId, List([testinvoiceLine, invoiceLineToRemove]));
+    const testInvoice = new Invoice(
+      testId,
+      testOwnerId,
+      List([testinvoiceLine, invoiceLineToRemove])
+    );
 
     expect(testInvoice.invoiceLines.count()).toBe(2);
 
     testInvoice.removeInvoiceLine(invoiceLineToRemove);
     expect(testInvoice.invoiceLines.count()).toBe(1);
-  })
+  });
 
   it('direct manipulation to invoice lines does not persist', () => {
     const testinvoiceLine = newInvoiceLine();
@@ -65,7 +87,7 @@ describe('invoice', () => {
 
     testInvoice.invoiceLines.concat(testinvoiceLine2);
     expect(testInvoice.invoiceLines.count()).toBe(1);
-  })
+  });
 
   it('adding an existing invoice line throws new error', () => {
     const testinvoiceLine = newInvoiceLine();
@@ -73,5 +95,5 @@ describe('invoice', () => {
     const testInvoice = new Invoice(testId, testOwnerId, List([testinvoiceLine]));
     expect(testInvoice.invoiceLines.count()).toBe(1);
     expect(() => testInvoice.addInvoiceLine(testinvoiceLine)).toThrow(InvoiceLineExistsError);
-  })
-})
+  });
+});
