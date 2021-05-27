852168651f95e7d3263cb0bf529cd77b38b72a58
Dominic Kim <rebyul@gmail.com>
2021-05-27 16:53:53 +1200

    Remove use of section and refactor home page

--- apps/sandwich-club/src/app/pages/home.page.tsx
+++ apps/sandwich-club/src/app/pages/home.page.tsx
@@ -1,35 +1,38 @@
-import { Container, Grid, makeStyles, Typography } from '@material-ui/core';
+import { Button, Container, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
 import React, { FC } from 'react';
-import { Section } from '../components/section/Section';
 
 const useStyles = makeStyles((theme) => ({
-  works: {
-    backgroundColor: theme.palette.background.paper,
-  },
   gridPaper: {
     backgroundSize: '35px 35px',
     backgroundImage: `linear-gradient(to right, #C0C0C0 1px, transparent 1px), linear-gradient(to bottom, #C0C0C0 1px, ${theme.palette.secondary.main} 1px)`,
   },
 }));
 
+const HomePageHero: FC<{ className?: string }> = ({ className, children }) => (
+  <Grid container direction="column" justify="center" alignItems="center" className={className}>
+    {children}
+  </Grid>
+);
+
 function HomePage() {
   const classes = useStyles();
 
   return (
     <>
-      <Grid container justify="center" alignItems="center" className={classes.gridPaper}>
+      <HomePageHero className={classes.gridPaper}>
         <img src="/assets/LunchBunch.png" height="auto" alt="premium-lunch-bunch" />
-      </Grid>
-      <Section />
-      <a href="">Enrol into this weeks Sandwich Club</a>
+        <Button>Join us this week</Button>
+      </HomePageHero>
       <br />
       <br />
-      <Section>
-        <Container className={classes.works} fixed>
-          <Typography variant="h2">How it works?</Typography>
-          <br />
-        </Container>
-      </Section>
+      <HomePageHero>
+        <Paper elevation={0}>
+          <Container>
+            <Typography variant="h2">How it works?</Typography>
+            <br />
+          </Container>
+        </Paper>
+      </HomePageHero>
       <br />
       <h2>reviews</h2>
     </>
