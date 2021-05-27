3577272b095583c1886c185a8fd706685f969665
Dominic Kim <rebyul@gmail.com>
2021-05-27 17:26:42 +1200

    Make button fonts bolder and larger

--- apps/sandwich-club/src/theme/typography.ts
+++ apps/sandwich-club/src/theme/typography.ts
@@ -3,6 +3,10 @@ import { lunchbunchPalette } from './colors';
 
 const lunchbunchTypography = createTypography(lunchbunchPalette, {
   fontFamily: '"Open Sans", "Helvetica", "Arial", sans-serif',
+  button: {
+    fontWeight: 700,
+    fontSize: '1rem',
+  },
 });
 
 export { lunchbunchTypography };
